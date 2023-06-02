3ee85e4072b6eb93d830d0f3b2a387fb9cca29d9
Lucasteisouza <lucasteisouza@gmail.com>
2023-06-02 17:03:07 -0300

    feat: implementa codigo para desafio 5

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,13 @@
 def find_duplicate(nums):
-    """Faça o código aqui."""
+    if len(nums) < 2:
+        return False
+    ordered_nums = sorted(nums)
+    for i in range(len(ordered_nums) - 1):
+        if type(ordered_nums[i]) != int or ordered_nums[i] < 0:
+            return False
+        if ordered_nums[i] == ordered_nums[i + 1]:
+            return ordered_nums[i]
+    return False
     raise NotImplementedError
+
+print(find_duplicate([1, 3, 4, 2, 2]))
